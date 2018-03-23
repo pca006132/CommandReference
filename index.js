9338d9fa7151e7267400a2c464d6e3456e331069
pca006132 <john.lck40@gmail.com>
2018-03-23 21:32:31 +0800

    used another validation library

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const json_schema = require("json-schema");
+const json_schema = require("jsonschema");
 const schema = require("./schema");
 const fs = require("fs");
+
 let data;
 try {
     data = JSON.parse(fs.readFileSync("./data.json"));
@@ -10,9 +11,10 @@ try {
     process.exit();
 }
 
-let result = json_schema.validate(data, schema);
-if (result.valid) {
-    console.log("OK!");
+let v = new json_schema.Validator();
+let result = v.validate(data, schema);
+if (result.errors.length === 0) {
+    console.log("OK");
 } else {
     for (let e of result.errors) {
         console.log(`Property: "${e.property}", Message: ${e.message}`);
