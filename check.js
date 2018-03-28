cc104d8dab35f629c248e91112527abc0c23cf68
pca006132 <john.lck40@gmail.com>
2018-03-28 16:48:43 +0800

    added sorting and formatting feature

--- check.js
+++ check.js
@@ -11,10 +11,38 @@ try {
     process.exit();
 }
 
+data["common-words"] = data["common-words"].sort((a, b) => {
+    if (a["缩写"] > b["缩写"])
+        return 1;
+    else if (a["缩写"] < b["缩写"])
+        return -1;
+    else
+        return 0;
+})
+
+for (let key of Object.keys(data.threads)) {
+    data.threads[key].tags = data.threads[key].tags.sort((a, b)=> {
+        let i = data.tags.indexOf(a);
+        let j = data.tags.indexOf(b);
+        if (i > j)
+            return 1;
+        else if (i < j)
+            return -1;
+        else
+            return 0;
+    });
+}
+
 let v = new json_schema.Validator();
 let result = v.validate(data, schema);
 if (result.errors.length === 0) {
-    console.log("OK");
+    fs.writeFile("./data.json", JSON.stringify(data, null, 4), (err)=> {
+        if (err) {
+            console.log("No problem in data, but error formatting the json file");
+        } else {
+            console.log("OK");
+        }
+    });
 } else {
     for (let e of result.errors) {
         console.log(`Property: "${e.property}", Message: ${e.message}`);
