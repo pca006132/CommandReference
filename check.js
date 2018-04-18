5801763a3ceb978e16aff95b43a784b81d3fabda
pca006132 <john.lck40@gmail.com>
2018-04-18 21:40:56 +0800

    updated tags, ready for auto generating tags

--- check.js
+++ check.js
@@ -21,6 +21,19 @@ data["common-words"] = data["common-words"].sort((a, b) => {
 })
 
 for (let key of Object.keys(data.threads)) {
+    let i = data.threads[key].tags.indexOf("预览版");
+    if (i >= 0) {
+        data.threads[key].tags.splice(i, 1);
+    }
+    i = data.threads[key].tags.indexOf("即将过时");
+    if (i >= 0) {
+        data.threads[key].tags.splice(i, 1);
+    }
+    i = data.threads[key].tags.indexOf("过时");
+    if (i >= 0) {
+        data.threads[key].tags.splice(i, 1);
+    }
+
     data.threads[key].tags = data.threads[key].tags.sort((a, b)=> {
         let i = data.tags.indexOf(a);
         let j = data.tags.indexOf(b);
